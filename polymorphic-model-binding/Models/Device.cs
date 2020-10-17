067e11a8259495dd77e7a97152f241e387e615fb
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-10-17 18:45:36 +0200

    Init

---
+++ polymorphic-model-binding/Models/Device.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace polymorphic_model_binding.Models
+{
+    public abstract class Device
+    {
+        public string Kind { get; set; }
+    }
+
+    public class Laptop : Device
+    {
+        public string CPUIndex { get; set; }
+    }
+
+    public class SmartPhone : Device
+    {
+        public string ScreenSize { get; set; }
+    }
+
+}
---
+++ polymorphic-model-binding/Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using polymorphic_model_binding.BinderProviders;
+using polymorphic_model_binding.Models;
+
+namespace polymorphic_model_binding.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [ModelBinder(BinderType = typeof(DeviceModelBinder))]
+        [BindProperty]
+        public Device Device { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Device = new Laptop { Kind = "Laptop", CPUIndex = "CPU" };
+        }
+
+        public void OnPost()
+        {
+
+        }
+    }
+}
